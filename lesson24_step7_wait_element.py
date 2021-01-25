3d022ae2f0a83ed381b0336fc8fe71e8c5f5cc6f
neznakomec1981 <neznakomec_1981@mail.ru>
2021-01-25 23:39:47 +0300

    new main file

---
+++ lesson24_step7_wait_element.py
@@ -0,0 +1,42 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+try:
+
+    browser = webdriver.Chrome()
+    # говорим WebDriver ждать все элементы в течение 5 секунд
+    # browser.implicitly_wait(12)
+
+    browser.get("http://suninjuly.github.io/explicit_wait2.html")
+
+    WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, 'price'), '$100')
+    )
+
+
+    button = browser.find_element_by_css_selector("button#book.btn")
+    button.click()
+
+    x_element = browser.find_element_by_css_selector('span#input_value')
+    x = x_element.text
+    y = calc(x)
+
+    input1 = browser.find_element_by_id('answer')
+    input1.send_keys(y)
+
+    button = browser.find_element_by_css_selector("button#solve.btn")
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
